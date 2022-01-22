d3df8830b52015cff3762f15a4900cbee0fc7842
Vikashirave <vikashirave@gmail.com>
2022-01-22 20:17:36 +0530

    project created

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAXfZswqrQ6dIC2tfmXxArGrPAbBUUjskk",
+    authDomain: "netflix-clone-vikashirave.firebaseapp.com",
+    projectId: "netflix-clone-vikashirave",
+    storageBucket: "netflix-clone-vikashirave.appspot.com",
+    messagingSenderId: "442224177665",
+    appId: "1:442224177665:web:20b71f8134cb591e790600"
+  };
