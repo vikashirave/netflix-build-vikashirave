3c17085c4052705c7bccc72fda9ae6e4254b3ca4
Vikashirave <vikashirave@gmail.com>
2022-01-25 19:14:33 +0530

    created sign in screen

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import React from 'react';
-import './App.css';
-import HomeScreen from './screens/HomeScreen';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
+import React from "react";
+import "./App.css";
+import HomeScreen from "./screens/HomeScreen";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import LoginScreen from "./screens/LoginScreen";
 function App() {
+  const user = null;
   return (
     <div className="app">
-     <Router>
-       <Switch>
-         <Route path="">
-           <HomeScreen />
-         </Route>
-       </Switch>
-     </Router>
-
+      <Router>
+        {!user ? (
+          <LoginScreen />
+        ) : (
+          <Switch>
+            <Route path="">
+              <HomeScreen />
+            </Route>
+          </Switch>
+        )}
+      </Router>
     </div>
   );
 }
