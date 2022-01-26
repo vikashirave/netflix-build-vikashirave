5743d460e6cb59680d42681cf8f933b7e08638e8
Vikashirave <vikashirave@gmail.com>
2022-01-26 12:22:37 +0530

    authentication

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,7 @@
+var firebase = require("firebase/app");
+require("firebase/auth");
+require("firebase/firestore");
+
 const firebaseConfig = {
     apiKey: "AIzaSyAXfZswqrQ6dIC2tfmXxArGrPAbBUUjskk",
     authDomain: "netflix-clone-vikashirave.firebaseapp.com",
@@ -6,3 +10,10 @@ const firebaseConfig = {
     messagingSenderId: "442224177665",
     appId: "1:442224177665:web:20b71f8134cb591e790600"
   };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+  const db = firebaseApp.firestore();
+  const auth = firebase.auth();
+
+  export { auth };
+  export default db;
